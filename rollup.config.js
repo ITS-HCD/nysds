393f3f19539b52e0acc9cbc1dcd202f68c4d3882
Jesse Gardner <jesse@plasticmind.com>
2024-09-09 22:21:32 -0400

    Delete Rollup plugin cleans /dist folder on each build

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import summary from 'rollup-plugin-summary';
 import replace from '@rollup/plugin-replace';
 import postcss from 'rollup-plugin-postcss';
+import del from 'rollup-plugin-delete';  
 
 export default {
   input: 'src/index.ts', // Main entry point for your library
@@ -22,6 +23,9 @@ export default {
       format: 'umd',
       name: 'excelsior',
       sourcemap: true,
+      globals: {
+        lit: 'Lit'
+      },
     }
   ],
   plugins: [
@@ -33,6 +37,11 @@ export default {
     resolve({
       extensions: ['.mjs', '.js', '.json', '.ts'],
     }),
+
+    // Delete plugin: Clean up the dist folder before building
+    del({ 
+      targets: 'dist/*' 
+    }),  
 
     // Summary plugin: Show bundle size after building
     summary(),
