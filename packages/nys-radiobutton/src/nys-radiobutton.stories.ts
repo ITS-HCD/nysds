c6d4cc0670ead649f67ef101b36960ed04726011
Emily Gorelik <emilygorelik2000@gmail.com>
2024-11-04 09:21:18 -0500

    screenreader works on windows with NDVA

--- packages/nys-radiobutton/src/nys-radiobutton.stories.ts
+++ packages/nys-radiobutton/src/nys-radiobutton.stories.ts
@@ -51,41 +51,45 @@ export const Editable: Story = {
     name: "office",
   },
   render: (args) => html`
-    <p>What is your primary work location?</p>
-    <nys-radiobutton
-      .checked=${args.checked}
-      .disabled=${args.disabled}
-      .label=${args.label}
-      .description=${args.description}
-      .name=${args.name}
-      .value=${args.value}
-    ></nys-radiobutton>
-    <nys-radiobutton
-      .checked=${false}
-      .disabled=${args.disabled}
-      .label=${"Manhatten"}
-      .description=${"New York City"}
-      .name=${args.name}
-      .value=${"manhatten"}
-    ></nys-radiobutton>
+    <div role="radiogroup">
+      <p>What is your primary work location?</p>
+      <nys-radiobutton
+        .checked=${args.checked}
+        .disabled=${args.disabled}
+        .label=${args.label}
+        .description=${args.description}
+        .name=${args.name}
+        .value=${args.value}
+      ></nys-radiobutton>
+      <nys-radiobutton
+        .checked=${false}
+        .disabled=${args.disabled}
+        .label=${"Manhatten"}
+        .description=${"New York City"}
+        .name=${args.name}
+        .value=${"manhatten"}
+      ></nys-radiobutton>
+    </div>
   `,
   parameters: {
     docs: {
       source: {
         code: `
-<p>What is your primary work location?</p>
-<nys-radiobutton
-  label="Albany"
-  description="Upstate New York"
-  name="office"
-  value="albany"
-></nys-radiobutton>
-<nys-radiobutton
-  label="Manhatten"
-  description="New York City"
-  name="office"
-  value="manhatten"
-></nys-radiobutton>`.trim(),
+<div role="radiogroup">
+  <p>What is your primary work location?</p>
+  <nys-radiobutton
+    label="Albany"
+    description="Upstate New York"
+    name="office"
+    value="albany"
+  ></nys-radiobutton>
+  <nys-radiobutton
+    label="Manhatten"
+    description="New York City"
+    name="office"
+    value="manhatten"
+  ></nys-radiobutton>
+</div>`.trim(),
         type: "auto",
       },
     },
@@ -103,54 +107,58 @@ export const Partial: Story = {
     name: "op-system",
   },
   render: (args) => html`
-    <p>Choose your preferred work operating system.</p>
-    <nys-radiobutton
-      .checked=${args.checked}
-      .disabled=${args.disabled}
-      .label=${args.label}
-      .description=${args.description}
-      .name=${args.name}
-      .value=${args.value}
-    ></nys-radiobutton>
-    <nys-radiobutton
-      .checked=${false}
-      .disabled=${false}
-      .label=${"Sequoia"}
-      .description=${"Macbook Air"}
-      .name=${args.name}
-      .value=${"mac"}
-    ></nys-radiobutton>
-    <nys-radiobutton
-      .checked=${false}
-      .disabled=${true}
-      .label=${"Linux"}
-      .name=${args.name}
-      .value=${"linux"}
-    ></nys-radiobutton>
+    <div role="radiogroup">
+      <p>Choose your preferred work operating system.</p>
+      <nys-radiobutton
+        .checked=${args.checked}
+        .disabled=${args.disabled}
+        .label=${args.label}
+        .description=${args.description}
+        .name=${args.name}
+        .value=${args.value}
+      ></nys-radiobutton>
+      <nys-radiobutton
+        .checked=${false}
+        .disabled=${false}
+        .label=${"Sequoia"}
+        .description=${"Macbook Air"}
+        .name=${args.name}
+        .value=${"mac"}
+      ></nys-radiobutton>
+      <nys-radiobutton
+        .checked=${false}
+        .disabled=${true}
+        .label=${"Linux"}
+        .name=${args.name}
+        .value=${"linux"}
+      ></nys-radiobutton>
+    </div>
   `,
   parameters: {
     docs: {
       source: {
         code: `
-<p>Choose your preferred work operating system.</p>
-<nys-radiobutton 
-  label="Windows 11"
-  description="HP Elitebook"
-  name="op-system"
-  value="windows"
-></nys-radiobutton>
-<nys-radiobutton
-  label="Sequoia"
-  description="Macbook Air"
-  name="op-system"
-  value="mac"
-></nys-radiobutton>
-<nys-radiobutton
-  disabled
-  label="Linux"
-  name="op-system"
-  value="windows"
-></nys-radiobutton>`.trim(),
+<div role="radiogroup">
+  <p>Choose your preferred work operating system.</p>
+  <nys-radiobutton 
+    label="Windows 11"
+    description="HP Elitebook"
+    name="op-system"
+    value="windows"
+  ></nys-radiobutton>
+  <nys-radiobutton
+    label="Sequoia"
+    description="Macbook Air"
+    name="op-system"
+    value="mac"
+  ></nys-radiobutton>
+  <nys-radiobutton
+    disabled
+    label="Linux"
+    name="op-system"
+    value="windows"
+  ></nys-radiobutton>
+</div>`.trim(),
         type: "auto",
       },
     },
@@ -168,68 +176,71 @@ export const Disabled: Story = {
     value: "eng-1",
   },
   render: (args) => html`
-    <p>
-      Current title. Note: You cannot change your title, if you believe you are
-      ready to be promoted talk to your supervisor.
-    </p>
-    <nys-radiobutton
-      .checked=${args.checked}
-      .disabled=${args.disabled}
-      .label=${args.label}
-      .description=${args.description}
-      .id=${args.id}
-      .name=${args.name}
-      .value=${args.value}
-    ></nys-radiobutton>
-    <nys-radiobutton
-      .checked=${false}
-      .disabled=${args.disabled}
-      .label=${"Software Engineer 2"}
-      .description=${"1-3 years experience"}
-      .id=${args.id}
-      .name=${args.name}
-      .value=${"eng-2"}
-    ></nys-radiobutton>
-    <nys-radiobutton
-      .checked=${false}
-      .disabled=${args.disabled}
-      .label=${"Software Engineer 3"}
-      .description=${"3-5 years experience"}
-      .id=${args.id}
-      .name=${args.name}
-      .value=${"eng-3"}
-    ></nys-radiobutton>
+    <div role="radiogroup">
+      <p>
+        Current title. Note: You cannot change your title, if you believe you
+        are ready to be promoted talk to your supervisor.
+      </p>
+      <nys-radiobutton
+        .checked=${args.checked}
+        .disabled=${args.disabled}
+        .label=${args.label}
+        .description=${args.description}
+        .id=${args.id}
+        .name=${args.name}
+        .value=${args.value}
+      ></nys-radiobutton>
+      <nys-radiobutton
+        .checked=${false}
+        .disabled=${args.disabled}
+        .label=${"Software Engineer 2"}
+        .description=${"1-3 years experience"}
+        .id=${args.id}
+        .name=${args.name}
+        .value=${"eng-2"}
+      ></nys-radiobutton>
+      <nys-radiobutton
+        .checked=${false}
+        .disabled=${args.disabled}
+        .label=${"Software Engineer 3"}
+        .description=${"3-5 years experience"}
+        .id=${args.id}
+        .name=${args.name}
+        .value=${"eng-3"}
+      ></nys-radiobutton>
+    </div>
   `,
   parameters: {
     docs: {
       source: {
         code: `
-<p>Current title. Note: You cannot change your title, if you believe you are ready to be promoted talk to your supervisor.</p>
-<nys-radiobutton
-  checked
-  disabled
-  label="Software Engineer 1"
-  description="<1 year experience"
-  name="title"
-  value="eng-1"
-></nys-radiobutton>
-<nys-radiobutton
-  checked
-  disabled
-  label="Software Engineer 2"
-  description="1-3 years experience"
-  name="title"
-  value="eng-2"
-></nys-radiobutton>
-<nys-radiobutton
-  checked
-  disabled
-  label="Software Engineer 3"
-  description="3-5 years experience"
-  name="title"
-  value="eng-3"
-></nys-radiobutton>
-        `.trim(),
+<div role="radiogroup">
+  <p>Current title. Note: You cannot change your title, if you believe you are ready to be promoted talk to your supervisor.</p>
+  <nys-radiobutton
+    checked
+    disabled
+    label="Software Engineer 1"
+    description="<1 year experience"
+    name="title"
+    value="eng-1"
+  ></nys-radiobutton>
+  <nys-radiobutton
+    checked
+    disabled
+    label="Software Engineer 2"
+    description="1-3 years experience"
+    name="title"
+    value="eng-2"
+  ></nys-radiobutton>
+  <nys-radiobutton
+    checked
+    disabled
+    label="Software Engineer 3"
+    description="3-5 years experience"
+    name="title"
+    value="eng-3"
+  ></nys-radiobutton>
+</div>`.trim(),
       },
     },
   },
