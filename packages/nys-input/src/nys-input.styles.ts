b57fa2b08f0f722d07e2cd8c797df8b9ce2ccfb1
Emily Gorelik <emilygorelik2000@gmail.com>
2024-11-06 10:18:31 -0500

    add initial styling

--- packages/nys-input/src/nys-input.styles.ts
+++ packages/nys-input/src/nys-input.styles.ts
@@ -1,3 +1,24 @@
 import { css } from "lit";
 
-export default css``;
+export default css`
+  .nys-input {
+    border-radius: 0.125rem;
+    border: solid 1px;
+    font-family: arial;
+    font-size: 16px;
+    padding: 0.25rem;
+  }
+
+  /* Focused */
+  .nys-input:focus {
+    outline-offset: 3px;
+    outline: 3px solid var(--form-focus-color, #007bff);
+  }
+
+  /* Disabled */
+  .nys-input:disabled {
+    background-color: var(--form-unchecked-color-bg-disabled, #f0f0f0);
+    border-color: var(--form-unchecked-color-border-disabled, #757575);
+    cursor: not-allowed;
+  }
+`;
