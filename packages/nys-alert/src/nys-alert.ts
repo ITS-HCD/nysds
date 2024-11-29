15561f7c4a6eb2e28660de495b911403c5bf0569
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2024-11-29 10:02:51 -0500

    Update nys-alert.ts

--- packages/nys-alert/src/nys-alert.ts
+++ packages/nys-alert/src/nys-alert.ts
@@ -1,7 +1,7 @@
 import { LitElement, html } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import styles from "./nys-alert.styles";
-import "/packages/nys-icon/dist/nys-icon.es.js"; // references: "/packages/nys-icon/dist/nys-icon.es.js";
+// import "/packages/nys-icon/dist/nys-icon.es.js"; // references: "/packages/nys-icon/dist/nys-icon.es.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 
 let alertIdCounter = 0; // Counter for generating unique IDs
