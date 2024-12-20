e25c9263a7cde7e7d42e6cd60bd150d8d5736b0d
Robert Chen <54908064+NovaCat35@users.noreply.github.com>
2024-12-20 14:53:26 -0500

    Toggle design revamp

--- packages/nys-toggle/src/nys-toggle.ts
+++ packages/nys-toggle/src/nys-toggle.ts
@@ -20,7 +20,7 @@ export class NysToggle extends LitElement {
   @property({ type: Boolean }) noIcon = false;
   @property({ type: String }) label = "";
   @property({ type: String }) description = "";
-  private static readonly VALID_SIZES = ["sm", "md", "lg"] as const;
+  private static readonly VALID_SIZES = ["sm", "md"] as const;
 
   // Private property to store the internal `size` value, restricted to the valid types. Default is "md".
   private _size: (typeof NysToggle.VALID_SIZES)[number] = "md";
@@ -128,7 +128,7 @@ export class NysToggle extends LitElement {
               </div>
             </span>
           </div>
-          <div class="nys-toggle__text">
+          <div class="nys-toggle__text ${this.disabled ? "disabled" : ""}">
             <div class="nys-toggle__label">${this.label}</div>
             <slot name="description">${this.description}</slot>
           </div>
