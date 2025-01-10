c02bd3da4395f1fc3af20555c54c4094d1bd6fe8
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-01-10 17:08:55 -0500

    Styles package includes utilities packages now (#179)

--- packages/nys-styles/vite.config.js
+++ packages/nys-styles/vite.config.js
@@ -6,13 +6,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         excelsior: "./src/excelsior.css",
-        "theme-admin": "./src/theme-admin.css",
-        "theme-business": "./src/theme-business.css",
-        "theme-environment": "./src/theme-environment.css",
-        "theme-health": "./src/theme-health.css",
-        "theme-local": "./src/theme-local.css",
-        "theme-safety": "./src/theme-safety.css",
-        "theme-transportation": "./src/theme-transportation.css",
+        "excelsior-theme-admin": "./src/theme-admin.css",
+        "excelsior-theme-business": "./src/theme-business.css",
+        "excelsior-theme-environment": "./src/theme-environment.css",
+        "excelsior-theme-health": "./src/theme-health.css",
+        "excelsior-theme-local": "./src/theme-local.css",
+        "excelsior-theme-safety": "./src/theme-safety.css",
+        "excelsior-theme-transportation": "./src/theme-transportation.css",
+        "excelsior-utility": "./src/utility.css",
+        "excelsior-utility-grid": "./src/utility-layout-grid.css",
       },
       output: {
         assetFileNames: "[name].min.css",
