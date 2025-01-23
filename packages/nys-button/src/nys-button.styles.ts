9709c7d786e58b2774874e9c799afe258195bfaa
Emily Gorelik <emilygorelik2000@gmail.com>
2025-01-23 12:58:37 -0500

    text button styled

--- packages/nys-button/src/nys-button.styles.ts
+++ packages/nys-button/src/nys-button.styles.ts
@@ -122,6 +122,31 @@ export default css`
     );
   }
   :host([variant="text"]) {
+    --_nys-button-height: fit-content;
+    --_nys-button-radius: var(--nys-border-radius-xl, 12px);
+    --_nys-button-padding-y: var(--nys-space-2px, 2px);
+    --_nys-button-padding-x: var(--nys-space-50, 4px);
+    --_nys-button-width-border: 0px;
+    --_nys-button-text-decoration: underline;
+
+    --_nys-button-color-bg: var(--nys-color-transparent, #ffffff00);
+    --_nys-button-color-text: var(--nys-color-link, #004dd1);
+    --_nys-button-color-border: var(--nys-color-transparent, #ffffff00);
+
+    --_nys-button-color-bg-hover: var(--nys-color-transparent, #ffffff00);
+    --_nys-button-color-text-hover: var(--nys-color-link-strong, #003ba1);
+    --_nys-button-color-border-hover: var(--nys-color-transparent, #ffffff00);
+
+    --_nys-button-color-bg-active: var(--nys-color-transparent, #ffffff00);
+    --_nys-button-color-text-active: var(--nys-color-link-strongest, #002971);
+    --_nys-button-color-border-active: var(--nys-color-transparent, #ffffff00);
+
+    --_nys-button-color-bg-disabled: var(--nys-color-transparent, #ffffff00);
+    --_nys-button-color-text-disabled: var(--nys-color-neutral-300, ##a7a9ab);
+    --_nys-button-color-border-disabled: var(
+      --nys-color-transparent,
+      #ffffff00
+    );
   }
 
   .nys-button {
@@ -135,6 +160,7 @@ export default css`
     font-size: var(--_nys-button-font-size);
     font-weight: var(--_nys-button-font-weight);
     line-height: var(--_nys-button-line-height);
+    text-decoration: var(--_nys-button-text-decoration);
     box-sizing: border-box;
     background-color: var(--_nys-button-color-bg);
     color: var(--_nys-button-color-text);
