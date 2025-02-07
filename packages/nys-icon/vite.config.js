4948836027ce481a5a68973baba687c45d5025b2
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-02-07 15:55:44 -0500

    Prep for 1.0.0 release

--- packages/nys-icon/vite.config.js
+++ packages/nys-icon/vite.config.js
@@ -8,7 +8,7 @@ const banner = `
    * ░█▀▀▀ ─░█── ░█─── ░█▀▀▀ ░█─── ─▀▀▀▄▄ ░█─ ░█──░█ ░█▄▄▀ 
    * ░█▄▄▄ ▄▀░▀▄ ░█▄▄█ ░█▄▄▄ ░█▄▄█ ░█▄▄▄█ ▄█▄ ░█▄▄▄█ ░█─░█
    * 
-   * Icon Component v0.0.9-alpha
+   * Icon Component v1.0.0
    * Part of New York State's Excelsior Design System
    * A design system for New York State's digital products.
    * Repository: https://github.com/its-hcd/excelsior
