c8be5d64293bf70559184405a37af35312fb3629
Robert Chen <bergintonatcapital@gmail.com>
2025-02-13 14:20:06 -0500

    Lint fix

--- packages/nys-unavfooter/vite.config.js
+++ packages/nys-unavfooter/vite.config.js
@@ -8,14 +8,8 @@ const banner = `
    * ▒█▒█▒█ ▒█▄▄▄█ ░▀▀▀▄▄ ▒█░▒█ ░▀▀▀▄▄ 
    * ▒█░░▀█ ░░▒█░░ ▒█▄▄▄█ ▒█▄▄▀ ▒█▄▄▄█
    * 
-<<<<<<< HEAD
-   * Universal Footer Component v1.0.0
-   * Part of New York State's Excelsior Design System
-   * (c) ${new Date().getFullYear()} New York State Design System Team
-=======
-   * Univeral Navigation Footer Component v1.0.0
+   * Universal Navigation Footer Component v1.0.0
    * Part of New York State's Design System
->>>>>>> 21a8638c2b8a2648a086acf254a08745311902f6
    * A design system for New York State's digital products.
    * Repository: https://github.com/its-hcd/nysds
    * License: MIT
