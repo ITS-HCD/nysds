8ab1e335d30507e195afe3d0445782bd0e816cd4
Robert Chen <bergintonatcapital@gmail.com>
2025-02-12 19:08:08 -0500

    Create first working interation of using ElementInternals

--- packages/nys-textinput/src/nys-textinput.ts
+++ packages/nys-textinput/src/nys-textinput.ts
@@ -1,7 +1,10 @@
 import { LitElement, html } from "lit";
 import { property } from "lit/decorators.js";
 import styles from "./nys-textinput.styles";
+import { ifDefined } from "lit/directives/if-defined.js";
 import "@nys-excelsior/nys-icon"; // references: "/packages/nys-icon/dist/nys-icon.es.js";
+
+let textinputIdCounter = 0; // Counter for generating unique IDs
 
 export class NysTextinput extends LitElement {
   @property({ type: String }) id = "";
@@ -61,16 +64,110 @@ export class NysTextinput extends LitElement {
   @property({ type: Boolean, reflect: true }) showError = false;
   @property({ type: String }) errorMessage = "";
 
-  constructor() {
-    super();
-  }
-
   static styles = styles;
 
+  // private _hasReportedValidity = false;
+  private _wasInvalid = false;
+  private _internals: ElementInternals;
+
+  /********************** Lifecycle updates **********************/
+  static formAssociated = true; // allows use of elementInternals' API
+
+  constructor() {
+    super();
+    this._internals = this.attachInternals();
+    console.log(this._internals);
+  }
+
+  // Generate a unique ID if one is not provided
+  connectedCallback() {
+    super.connectedCallback();
+    if (!this.id) {
+      this.id = `nys-textinput-${Date.now()}-${textinputIdCounter++}`;
+    }
+  }
+
+  firstUpdated() {
+    // This ensures our element always participates in the form
+    this._setValue();
+    this._manageRequire();
+  }
+
+  /********************** Form Integration **********************/
+  private _setValue() {
+    this._internals.setFormValue(this.value);
+  }
+
+  private _manageRequire() {
+    const input = this.shadowRoot?.querySelector("input");
+    if (!input) return;
+
+    if (this.required) {
+      this._internals.ariaRequired = "true";
+      this._internals.setValidity({ valueMissing: true }, 'This field is required', input);
+    } else {
+      this._internals.setValidity({});
+    }
+    // Force revalidation to update the form's state
+    input.checkValidity();
+  }
+
+  setCustomValidity(message: string = "") {
+    const input = this.shadowRoot?.querySelector("input");
+    if (!input) return;
+
+    // errorMessage overrides any native error message
+    this.errorMessage = this.errorMessage.trim() ? this.errorMessage : message;
+    this.showError = !!message;
+
+    console.log("User value: ", this.value);
+    console.log("The validity is: ", input.checkValidity());
+    console.log("The message is: ", message);
+    console.log("The errorMsg is: ", this.errorMessage);
+    console.log("test: ", this.errorMessage || message);
+    console.log("test2: ", !!message);
+    console.log("test3: ", this.showError);
+
+    this._internals.setValidity(message ? { customError: true } : {}, message);
+
+    // Force revalidation to update the form's state
+    input.checkValidity();
+    // input.reportValidity();
+  }
+
+  private _validate() {
+    const input = this.shadowRoot?.querySelector("input");
+    const regexTest = new RegExp(this.pattern).test(this.value)
+    if (!input) return;
+
+    // Required and value is empty - validation fails
+    if (this.required && !this.value) {
+      this.setCustomValidity("This field is required");
+      return;
+    }
+
+    // Pattern exist and regex match fails
+    if (this.pattern && !regexTest) {
+      this.setCustomValidity("Invalid format");
+      return;
+    }
+
+    // Reset error if no validation errors
+    this.setCustomValidity();
+  }
+
+  /******************** Event Handlers ********************/
   // Handle input event to check pattern validity
   private _handleInput(event: Event) {
     const input = event.target as HTMLInputElement;
     this.value = input.value;
+    this._internals.setFormValue(this.value);
+
+    // If the field was invalid, validate aggressively on each input (e.g. Eager mode: a combination of aggressive and lazy.)
+    if (this._wasInvalid) {
+      this._validate();
+    }
+
     this.dispatchEvent(
       new CustomEvent("input", {
         detail: { value: this.value },
@@ -78,6 +175,9 @@ export class NysTextinput extends LitElement {
         composed: true,
       }),
     );
+
+
+    // DELETE BELOW ~~~~~!!!!!
     const checkValidity = input.checkValidity();
     this.dispatchEvent(
       new CustomEvent("nys-checkValidity", {
@@ -95,20 +195,29 @@ export class NysTextinput extends LitElement {
 
   // Handle blur event
   private _handleBlur() {
+    console.log('repeating?????')
+    this._validate();
+    if (this.showError) {
+      this._wasInvalid = true; // If invalid, start aggressive mode
+    }
     this.dispatchEvent(new Event("blur"));
   }
 
   // Handle change event
-  private _handleChange(e: Event) {
-    const select = e.target as HTMLSelectElement;
-    this.value = select.value;
-    this.dispatchEvent(
-      new CustomEvent("change", {
-        detail: { value: this.value },
-        bubbles: true,
-        composed: true,
-      }),
-    );
+  private _handleChange() {
+    // DELETE BELOW ~~~~~!!!!!
+    // const select = e.target as HTMLSelectElement;
+    // this.value = select.value;
+    // this.setCustomValidity(); // Validate on change
+
+    // this.dispatchEvent(
+    //   new CustomEvent("change", {
+    //     detail: { value: this.value },
+    //     bubbles: true,
+    //     composed: true,
+    //   }),
+    // );
+    this.dispatchEvent(new Event("change"));
   }
 
   render() {
@@ -142,12 +251,12 @@ export class NysTextinput extends LitElement {
           aria-label="${this.label} ${this.description}"
           .value=${this.value}
           placeholder=${this.placeholder}
-          maxlength=${this.maxlength}
-          pattern=${this.pattern}
-          step=${this.step}
-          min=${this.min}
-          max=${this.max}
-          form=${this.form}
+          maxlength=${ifDefined(this.maxlength ? this.maxlength : undefined)}
+          pattern=${ifDefined(this.pattern ? this.pattern : undefined)}
+          step=${ifDefined(this.step ? this.step : undefined)}
+          min=${ifDefined(this.min ? this.min : undefined)}
+          max=${ifDefined(this.max ? this.max : undefined)}
+          form=${ifDefined(this.form ? this.form : undefined)}
           @input=${this._handleInput}
           @focus="${this._handleFocus}"
           @blur="${this._handleBlur}"
@@ -156,7 +265,7 @@ export class NysTextinput extends LitElement {
         ${this.showError && this.errorMessage
           ? html`<div class="nys-textinput__error">
               <nys-icon name="error" size="xl"></nys-icon>
-              ${this.errorMessage}
+              ${this._internals.validationMessage}
             </div>`
           : ""}
       </div>
