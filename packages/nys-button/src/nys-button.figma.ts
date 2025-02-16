99bf5e15045606622a961c7003f640946c00e463
Jesse Gardner <jesse@plasticmind.com>
2025-02-15 21:32:50 -0500

    Updated all props for buttons

--- packages/nys-button/src/nys-button.figma.ts
+++ packages/nys-button/src/nys-button.figma.ts
@@ -5,25 +5,34 @@ figma.connect("<FIGMA_BUTTONS_BUTTON>", {
     labelText: figma.string("Label Text"),
     size: figma.enum("Size", {
       sm: "sm",
-      md: "md",
       lg: "lg",
     }),
     variant: figma.enum("Variant", {
-      Filled: "filled",
-      Outline: "outline",
-      Ghost: "ghost",
-      Text: "text",
+      outline: "outline",
+      ghost: "ghost",
+      text: "text",
     }),
     inverted: figma.boolean("Inverted"),
     disabled: figma.boolean("Disabled"),
+    /*
+    prefixIcon: figma.instance("Prefix Icon"),
+    suffixIcon: figma.instance("Suffix Icon"),
+    
+    prefixIcon: figma.nestedProps("Prefix Icon", {
+      shape: figma.string("shape"),
+    }),
+    suffixIcon: figma.nestedProps("Suffix Icon", {
+      shape: figma.string("shape"),
+    }),
+    */
   },
   example: (props) => html`
     <nys-button
       label="${props.labelText}"
       size="${props.size}"
       variant="${props.variant}"
-      ?inverted="${props.inverted}"
-      ?disabled="${props.disabled}"
+      inverted="${props.inverted}"
+      disabled="${props.disabled}"
     ></nys-button>
   `,
 });
