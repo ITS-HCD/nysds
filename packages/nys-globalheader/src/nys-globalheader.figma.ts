39a687f45b3e94802f62dc9125a0b3156876b575
Jesse Gardner <jesse@plasticmind.com>
2025-02-17 05:26:56 -0500

    Added CodeConnect for nys-globalheader

---
+++ packages/nys-globalheader/src/nys-globalheader.figma.ts
@@ -0,0 +1,56 @@
+import figma, { html } from "@figma/code-connect/html";
+
+// Variant: Application Name Only
+figma.connect("<FIGMA_GLOBALHEADER>", {
+  variant: { Content: "Application" },
+  props: {
+    appName: figma.string("Application Name"),
+  },
+  example: (props) => html`
+    <nys-globalheader appName="${props.appName}">
+      <!-- Placeholder, replace with real agency links -->
+      <ul>
+        <li><a href="https://">Placeholder</a></li>
+        <li><a href="https://">Placeholder</a></li>
+      </ul>
+    </nys-globalheader>
+  `,
+});
+
+// Variant: Agency Name Only
+figma.connect("<FIGMA_GLOBALHEADER>", {
+  variant: { Content: "Agency" },
+  props: {
+    agencyName: figma.string("Agency Name"),
+  },
+  example: (props) => html`
+    <nys-globalheader agencyName="${props.agencyName}">
+      <!-- Placeholder, replace with real agency links -->
+      <ul>
+        <li><a href="https://">Placeholder</a></li>
+        <li><a href="https://">Placeholder</a></li>
+      </ul>
+    </nys-globalheader>
+  `,
+});
+
+// Variant: Application + Agency Name
+figma.connect("<FIGMA_GLOBALHEADER>", {
+  variant: { Content: "Application + Agency" },
+  props: {
+    appName: figma.string("Application Name"),
+    agencyName: figma.string("Agency Name"),
+  },
+  example: (props) => html`
+    <nys-globalheader
+      appName="${props.appName}"
+      agencyName="${props.agencyName}"
+    >
+      <!-- Placeholder, replace with real agency links -->
+      <ul>
+        <li><a href="https://">Placeholder</a></li>
+        <li><a href="https://">Placeholder</a></li>
+      </ul>
+    </nys-globalheader>
+  `,
+});
