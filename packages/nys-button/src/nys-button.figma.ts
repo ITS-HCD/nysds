6fe44957bbe571b4d99413015adacfcb3f4794da
Jesse Gardner <jesse@plasticmind.com>
2025-03-03 07:39:30 -0500

    Updated Code Connect for nys-button

--- packages/nys-button/src/nys-button.figma.ts
+++ packages/nys-button/src/nys-button.figma.ts
@@ -5,37 +5,56 @@ figma.connect("<FIGMA_BUTTON>", {
     labelText: figma.string("Label Text"),
     size: figma.enum("Size", {
       sm: "sm",
+      // md: "md",
       lg: "lg",
     }),
     variant: figma.enum("Variant", {
-      outline: "Outline",
-      ghost: "Ghost",
-      text: "Text",
+      Outline: "outline",
+      Ghost: "ghost",
+      Text: "text",
     }),
     inverted: figma.boolean("Inverted"),
     disabled: figma.boolean("Disabled"),
-    prefixIcon: figma.instance("Prefix Icon"),
-    suffixIcon: figma.instance("Suffix Icon"),
+    prefixIcon: figma.boolean("Prefix Icon", {
+      true: "icon_name_REPLACE",
+      false: undefined,
+    }),
+    suffixIcon: figma.boolean("Suffix Icon", {
+      true: "icon_name_REPLACE",
+      false: undefined,
+    }),
   },
   example: (props) => html`
+    <!-- 
+  - Use fullWidth attribute to fill parent container 
+  - Update 'type' attribute to change button type: submit, reset, button (default)
+  - Update 'form' attribute to associate with a form
+  - Add 'href' attribute to make it a link
+  - ⚠️ Icon names are not populated, replace with actual icon names
+-->
     <nys-button
       label="${props.labelText}"
       size="${props.size}"
       variant="${props.variant}"
       inverted="${props.inverted}"
       disabled="${props.disabled}"
+      prefixIcon="${props.prefixIcon}"
+      suffixIcon="${props.suffixIcon}"
     ></nys-button>
   `,
 });
 
 /*
-    prefixIcon: figma.instance("Prefix Icon"),
-    suffixIcon: figma.instance("Suffix Icon"),
-    
-    prefixIcon: figma.nestedProps("Prefix Icon", {
-      shape: figma.string("shape"),
-    }),
-    suffixIcon: figma.nestedProps("Suffix Icon", {
-      shape: figma.string("shape"),
-    }),
-    */
+prefixIcon: figma.instance("Prefix Icon"),
+suffixIcon: figma.instance("Suffix Icon"),
+
+prefixIcon: figma.nestedProps("Prefix Icon", {
+  shape: figma.string("shape"),
+}),
+suffixIcon: figma.nestedProps("Suffix Icon", {
+  shape: figma.string("shape"),
+}),
+
+prefixIcon="${props.prefixIcon.shape}"
+suffixIcon="${props.suffixIcon.shape}"
+*/
