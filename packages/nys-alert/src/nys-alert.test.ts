88ed5d4d7abb14d8a311a50dd6ef7b8034f55313
Emily Gorelik <emilygorelik2000@gmail.com>
2025-04-15 13:37:19 -0400

    test runs but failed

--- packages/nys-alert/src/nys-alert.test.ts
+++ packages/nys-alert/src/nys-alert.test.ts
@@ -1,20 +1,15 @@
 import { expect, html, fixture } from "@open-wc/testing";
-// import { NysAlert } from "./nys-alert"; 
+import { NysAlert } from "./nys-alert";
 // import "./nys-alert";
 
 describe("nys-alert", () => {
   // FAIL THE TEST (this is me testing out the errors)
-  it("should NOT pass this basic test", () => {
-    expect(true).to.be.false;
-  });
-
-  it("should render component", async () => {
-    const el = await fixture(html`<nys-alert></nys-alert>`);
-    expect(el).to.exist;
-  });
-
-  // it("should have default type as base", async () => {
-  //   const el = await fixture<NysAlert>(html`<nys-alert></nys-alert>`);
-  //   expect(el.type).to.equal("base");
+  // it("should NOT pass this basic test", () => {
+  //   expect(true).to.be.false;
   // });
+
+  it("should have default type as base", async () => {
+    const el = await fixture<NysAlert>(html`<nys-alert></nys-alert>`);
+    expect(el.type).to.equal("base");
+  });
 });
