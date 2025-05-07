98f4c54e81452d150c073535977d32fd609dab0f
NovaCat35 <bergintonatcapital@gmail.com>
2025-05-06 23:04:32 -0400

    Fix issue with voiceover leaving lingering black outline due to focus state not changing to main-content, just scrolling to view

--- packages/nys-skipnav/src/nys-skipnav.ts
+++ packages/nys-skipnav/src/nys-skipnav.ts
@@ -2,8 +2,6 @@ import { LitElement, html } from "lit";
 import { property } from "lit/decorators.js";
 import styles from "./nys-skipnav.styles";
 
-let skipnavIdCounter = 0; // Counter for generating unique IDs
-
 export class NysSkipnav extends LitElement {
   @property({ type: String }) id = "";
   @property({ type: String }) href = "";
@@ -19,7 +17,7 @@ export class NysSkipnav extends LitElement {
   connectedCallback() {
     super.connectedCallback();
     if (!this.id) {
-      this.id = `nys-skipnav-${Date.now()}-${skipnavIdCounter++}`;
+      this.id = `nys-skipnav-${Date.now()}`;
     }
   }
 
@@ -40,6 +38,20 @@ export class NysSkipnav extends LitElement {
     }
   }
 
+  private _handleClick() {
+    // Use href or default to #main-content
+    const targetId = (this.href || "#main-content").replace("#", "");
+    const targetEl = document.getElementById(targetId);
+  
+    if (targetEl) {
+      // Make sure it's focusable for screen readers rather than just scrolling to it
+      targetEl.setAttribute("tabindex", "-1");
+      targetEl.focus();
+      // Remove the blue outline that is applied to focused elements since we don't want the blue outline on the main container
+      targetEl.style.outline = "none";
+    }
+  }
+
   render() {
     return html`
       <div class="nys-skipnav">
@@ -50,6 +62,7 @@ export class NysSkipnav extends LitElement {
           class="nys-skipnav__link ${this.demoVisible ? "demo" : ""}"
           @focus="${this._handleFocus}"
           @blur="${this._handleBlur}"
+          @click="${this._handleClick}"
         >
           Skip to main content
         </a>
