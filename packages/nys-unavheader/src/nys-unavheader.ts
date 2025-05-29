f576869ef1cad89b6b7d5f28b9cf672084a4a51a
Emily Gorelik <emilygorelik2000@gmail.com>
2025-05-29 12:13:31 -0400

    no need to double check search not empty

--- packages/nys-unavheader/src/nys-unavheader.ts
+++ packages/nys-unavheader/src/nys-unavheader.ts
@@ -103,8 +103,7 @@ export class NysUnavHeader extends LitElement {
   }
 
   private _handleSearch(searchValue: string) {
-    if (searchValue !== "")
-      window.location.href = `https://search.its.ny.gov/search/search.html?btnG=Search&client=default_frontend&output=xml_no_dtd&proxystylesheet=default_frontend&ulang=en&sort=date:D:L:d1&entqr=3&entqrm=0&wc=200&wc_mc=1&oe=UTF-8&ie=UTF-8&ud=1&site=default_collection&q=${searchValue}+inurl:${window.location.hostname}&site=default_collection`;
+    window.location.href = `https://search.its.ny.gov/search/search.html?btnG=Search&client=default_frontend&output=xml_no_dtd&proxystylesheet=default_frontend&ulang=en&sort=date:D:L:d1&entqr=3&entqrm=0&wc=200&wc_mc=1&oe=UTF-8&ie=UTF-8&ud=1&site=default_collection&q=${searchValue}+inurl:${window.location.hostname}&site=default_collection`;
   }
 
   connectedCallback() {
