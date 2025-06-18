fc6ca509ffa63fb02c707e0f2fd0cf6e91f51f61
NovaCat35 <bergintonatcapital@gmail.com>
2025-06-18 15:43:46 -0400

    Add figma file and test

--- packages/nys-tooltip/src/nys-tooltip.figma.ts
+++ packages/nys-tooltip/src/nys-tooltip.figma.ts
@@ -2,8 +2,20 @@ import figma, { html } from "@figma/code-connect/html";
 
 figma.connect("<FIGMA_TOOLTIP>", {
   props: {
-    // TODO: Add props here
-
+    text: figma.string("Text"),
+    inverted: figma.boolean("Inverted"),
+    position: figma.enum("Position", {
+      top: "top",
+      bottom: "bottom",
+      left: "left",
+      right: "right",
+    }),
   },
-  example: () => html` <nys-tooltip></nys-tooltip> `,
+  example: (props) => html`
+    <nys-tooltip
+      text="${props.text}"
+      inverted="${props.inverted}"
+      position="${props.position}"
+    ></nys-tooltip>
+  `,
 });
--- packages/nys-tooltip/src/nys-tooltip.test.ts
+++ packages/nys-tooltip/src/nys-tooltip.test.ts
@@ -1,37 +1,44 @@
 import { expect, html, fixture } from "@open-wc/testing";
+import { NysTooltip } from "./nys-tooltip";
 import "../dist/nys-tooltip.js";
-// You may need to import other dependencies such as the component's tag name
-// For example:
-// import { NysTextinput } from "./nys-textinput";
 
 // Below are placeholder examples of test cases for a web component. Add your own tests as needed.
 describe("nys-tooltip", () => {
   it("renders the component", async () => {
-    const el = await fixture(html`<nys-tooltip></nys-tooltip>`);
+    const el = await fixture(
+      html`<nys-tooltip text="Helpful Tip."></nys-tooltip>`,
+    );
     expect(el).to.exist;
   });
 
-
   it("reflects attributes to properties", async () => {
-    const el = await fixture(html`
-      <nys-tooltip label="My Label" required optional></nys-tooltip>
+    const el = await fixture<NysTooltip>(html`
+      <nys-tooltip text="My Text" position="top" inverted></nys-tooltip>
     `);
-    expect(el.label).to.equal("My Label");
-    expect(el.required).to.be.true;
-    expect(el.optional).to.be.true;
+    expect(el.text).to.equal("My Text");
+    expect(el.position).to.equal("top");
+    expect(el.inverted).to.be.true;
+  });
+
+  it("renders slotted content", async () => {
+    const el = await fixture<NysTooltip>(
+      html`<nys-tooltip><button>Click</button></nys-tooltip>`,
+    );
+    const button = el.shadowRoot!.querySelector("slot")!;
+    expect(button).to.exist;
+  });
+
+  /*** Accessibility tests ***/
+  it("renders tooltip content with role=tooltip", async () => {
+    const el = await fixture<NysTooltip>(
+      html`<nys-tooltip text="Info"></nys-tooltip>`,
+    );
+    const tooltip = el.shadowRoot?.querySelector('[role="tooltip"]');
+    expect(tooltip).to.exist;
   });
 
   it("passes the a11y audit", async () => {
-    const el = await fixture(html`<nys-tooltip label="My Label"></nys-tooltip>`);
+    const el = await fixture(html`<nys-tooltip text="My Text"></nys-tooltip>`);
     await expect(el).shadowDom.to.be.accessible();
   });
-
-  // Other test to consider:
-  // - Test for default values
-  // - Test for different attributes
-  // - Test for events
-  // - Test for methods
-  // - Test for accessibility
-  // - Test for slot content
-  // - Test for lifecycle methods
-})
+});
