72398b1213ee15d4ac042b52d0ec52cf927fecef
Emily Gorelik <emilygorelik2000@gmail.com>
2025-08-13 14:39:53 -0400

    add code connect

--- packages/nys-badge/src/nys-badge.figma.ts
+++ packages/nys-badge/src/nys-badge.figma.ts
@@ -2,7 +2,30 @@ import figma, { html } from "@figma/code-connect/html";
 
 figma.connect("<FIGMA_BADGE>", {
   props: {
-    // TODO: Add props here
+    intent: figma.enum("Intent", {
+      "🔵 Info": "info",
+      "🟢 Success": "success",
+      "🟡 Warning": "warning",
+      "🔴 Error": "error",
+      "🔘 Neutral": "neutral",
+    }),
+    size: figma.enum("Size", {
+      sm: "sm",
+      md: "sm",
+    }),
+    prefixIcon: figma.boolean("Prefix Icon"),
+    suffixIcon: figma.boolean("Suffix Icon"),
+    prefix: figma.string("Prefix"),
+    label: figma.string("Label"),
   },
-  example: () => html` <nys-badge></nys-badge> `,
+  example: (props) => html`
+    <nys-badge
+      label="${props.label}"
+      intent="${props.intent}"
+      size="${props.size}"
+      ?prefixIcon="${props.prefixIcon}"
+      ?suffixIcon="${props.suffixIcon}"
+      prefix="${props.prefix}"
+    ></nys-badge>
+  `,
 });
