e6b80bce8305c59b5e730502684ecb95a5caf8ce
NovaCat35 <bergintonatcapital@gmail.com>
2025-08-21 15:35:08 -0400

    Revisions

--- packages/nys-button/src/nys-button.figma.ts
+++ packages/nys-button/src/nys-button.figma.ts
@@ -43,37 +43,3 @@ figma.connect("<FIGMA_BUTTON>", {
     ></nys-button>
   `,
 });
-
-// figma.connect("https://www.figma.com/design/0ogYpymUPQQfhELthntNbt/%F0%9F%92%A0-NYSDS-%2F-Components?m=auto&node-id=2205-1627&t=hlgC7mfYt6d2hrE9-1", {
-//   props: { icon: figma.instance("shape") },
-//   example: (props) => html`${props.icon}`,
-// });
-
-// figma.connect("<FIGMA_ICON_FIXED>", {
-//   props: { icon: figma.instance("shape") },
-//   example: (props) => html`${props.icon}`,
-// });
-
-/*
-prefixIcon: figma.instance("Prefix Icon"),
-suffixIcon: figma.instance("Suffix Icon"),
-
-prefixIcon: figma.nestedProps("Prefix Icon", {
-  shape: figma.string("shape"),
-}),
-suffixIcon: figma.nestedProps("Suffix Icon", {
-  shape: figma.string("shape"),
-}),
-
-prefixIcon="${props.prefixIcon.shape}"
-suffixIcon="${props.suffixIcon.shape}"
-
-    prefixIcon: figma.boolean("Prefix Icon", {
-      true: figma.children("Prefix Icon"),
-      false: undefined,
-    }),
-    suffixIcon: figma.boolean("Suffix Icon", {
-      true: figma.children("Suffix Icon"),
-      false: undefined,
-    }),
-*/
