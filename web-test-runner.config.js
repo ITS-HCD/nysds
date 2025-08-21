049a493ba5d843ec022b4855f1bd58778ae98cde
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-08-21 15:33:00 -0400

    Prep for 1.7.0

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,30 +1,45 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 import { playwrightLauncher } from "@web/test-runner-playwright";
 
+// Firefox declared the Lit is in DEV mode. this filters out that message to reduce the chatter in the testing logs
+const filteredLogs = [
+  'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.'
+]
+
+const filterBrowserLogs = (log) => {
+  for (const arg of log.args) {
+    if (typeof arg === 'string' && filteredLogs.some(l => arg.includes(l))) {
+      return false
+    }
+  }
+  return true
+}
+
 export default {
   files: ["packages/**/*.test.ts", "src/**/*.test.ts"],
   nodeResolve: true,
-  concurrency: 1,
+  filterBrowserLogs,
+  concurrency: 4,
   browsers: [
     playwrightLauncher({
       product: "chromium",
       launchOptions: {
-        headless: true, // Forces the browser to open in non-headless mode
-        slowMo: 250, // Optional: slows down operations to make debugging easier
+        headless: true, // Forces the browser to open in headless mode
+        // slowMo: 250, // Optional: slows down operations to make debugging easier
       },
     }),
     playwrightLauncher({
       product: "webkit",
       launchOptions: {
         headless: true, // Forces the browser to open in non-headless mode
-        slowMo: 250, // Optional: slows down operations to make debugging easier
+        // slowMo: 250, // Optional: slows down operations to make debugging easier
       },
     }),
     playwrightLauncher({
       product: "firefox",
       launchOptions: {
         headless: true, // Forces the browser to open in non-headless mode
-        slowMo: 250, // Optional: slows down operations to make debugging easier
+        // slowMo: 250, // Optional: slows down operations to make debugging easier
       },
     }),
   ],
