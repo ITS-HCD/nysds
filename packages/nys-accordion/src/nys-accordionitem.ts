8a333dd2e7a8fd8ae00b1e15599ce4055e05271c
NovaCat35 <bergintonatcapital@gmail.com>
2025-08-26 10:01:16 -0400

    Fix accordion not expanding slot issue

--- packages/nys-accordion/src/nys-accordionitem.ts
+++ packages/nys-accordion/src/nys-accordionitem.ts
@@ -26,6 +26,17 @@ export class NysAccordionItem extends LitElement {
     }
   }
 
+  firstUpdated() {
+    const slot = this.shadowRoot?.querySelector("slot");
+
+    console.log("HERE WE ARE");
+    if (this.expanded && slot) {
+      slot.addEventListener("slotchange", () => {
+        this._updateHeight();
+      });
+    }
+  }
+
   updated(changedProperties: Map<string, any>) {
     if (changedProperties.has("expanded")) {
       this._updateHeight();
