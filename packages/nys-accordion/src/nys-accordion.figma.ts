6f7ef7969fdc077ee565ac0f0f23f40930b5c87e
NovaCat35 <bergintonatcapital@gmail.com>
2025-09-02 15:26:33 -0400

    Fix accordion incorrect prop

--- packages/nys-accordion/src/nys-accordion.figma.ts
+++ packages/nys-accordion/src/nys-accordion.figma.ts
@@ -19,18 +19,13 @@ figma.connect("<FIGMA_ACCORDION>", {
   props: {
     singleSelect: figma.boolean("Single Select"),
     bordered: figma.boolean("Bordered"),
-    expanded: figma.boolean("Expanded"),
   },
   example: (props) => html`
     <nys-accordion
       singleSelect=${props.singleSelect}
       bordered=${props.bordered}
     >
-      <nys-accordionitem
-        id="accordion1"
-        heading="Accordion One"
-        ${props.expanded}
-      >
+      <nys-accordionitem id="accordion1" heading="Accordion One">
         <p>This is the content of accordion one.</p>
       </nys-accordionitem>
       <nys-accordionitem id="accordion2" heading="Accordion Two">
