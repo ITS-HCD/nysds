653b76cdd08a97abbed4110ec63316793355ac24
NovaCat35 <bergintonatcapital@gmail.com>
2025-09-02 16:36:25 -0400

    Separate subcomponent links

--- packages/nys-accordion/src/nys-accordion.figma.ts
+++ packages/nys-accordion/src/nys-accordion.figma.ts
@@ -1,6 +1,51 @@
 import figma, { html } from "@figma/code-connect/html";
 
-figma.connect("<FIGMA_ACCORDIONITEM>", {
+figma.connect("<FIGMA_ACCORDIONITEM1>", {
+  props: {
+    heading: figma.string("Heading"),
+    expanded: figma.boolean("Expanded"),
+  },
+  example: (props) => html`
+    <nys-accordionitem heading="${props.heading}" expanded=${props.expanded}>
+      <p>
+        This is example accordion content. Customize the content inside the
+        slot.
+      </p>
+    </nys-accordionitem>
+  `,
+});
+
+figma.connect("<FIGMA_ACCORDIONITEM2>", {
+  props: {
+    heading: figma.string("Heading"),
+    expanded: figma.boolean("Expanded"),
+  },
+  example: (props) => html`
+    <nys-accordionitem heading="${props.heading}" expanded=${props.expanded}>
+      <p>
+        This is example accordion content. Customize the content inside the
+        slot.
+      </p>
+    </nys-accordionitem>
+  `,
+});
+
+figma.connect("<FIGMA_ACCORDIONITEM3>", {
+  props: {
+    heading: figma.string("Heading"),
+    expanded: figma.boolean("Expanded"),
+  },
+  example: (props) => html`
+    <nys-accordionitem heading="${props.heading}" expanded=${props.expanded}>
+      <p>
+        This is example accordion content. Customize the content inside the
+        slot.
+      </p>
+    </nys-accordionitem>
+  `,
+});
+
+figma.connect("<FIGMA_ACCORDIONITEM4>", {
   props: {
     heading: figma.string("Heading"),
     expanded: figma.boolean("Expanded"),
