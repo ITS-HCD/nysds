f7a864913d74ef2cacac8f5c9f0c9101694b2a11
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-09-04 15:54:12 -0400

    Prep for 1.8.0 - Adirondack

--- plopfile.js
+++ plopfile.js
@@ -19,7 +19,7 @@ export default function (plop) {
         type: "input",
         name: "versionNumber",
         message: "Version Number",
-        default: "1.7.0", //update this to the latest version when new release is made
+        default: "1.8.0", //update this to the latest version when new release is made
       },
     ],
     actions: [
