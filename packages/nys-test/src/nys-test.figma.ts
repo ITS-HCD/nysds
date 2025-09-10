31cf13b1e03b6f1af7c13e12a83fd9028e4896f1
NovaCat35 <bergintonatcapital@gmail.com>
2025-09-10 16:07:17 -0400

    Remove test folder

--- packages/nys-test/src/nys-test.figma.ts
+++
@@ -1,8 +0,0 @@
-import figma, { html } from "@figma/code-connect/html";
-
-figma.connect("<FIGMA_TEST>", {
-  props: {
-    // TODO: Add props here
-  },
-  example: () => html` <nys-test></nys-test> `,
-});
