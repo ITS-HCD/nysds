06a955b49ec9d64afcaa6db04469418e23d0fe32
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-10-31 09:49:16 -0400

    Prep for 1.11.0

--- plopfile.js
+++ plopfile.js
@@ -19,7 +19,7 @@ export default function (plop) {
         type: "input",
         name: "versionNumber",
         message: "Version Number",
-        default: "1.10.1", //update this to the latest version when new release is made
+        default: "1.11.0", //update this to the latest version when new release is made
       },
       {
         type: "confirm",
