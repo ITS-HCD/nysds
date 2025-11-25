97ac4d5926f292e27ee9e8309e382c7bba40efd1
NovaCat35 <bergintonatcapital@gmail.com>
2025-11-25 11:37:46 -0500

    Realign checkbox figma code connect

--- packages/nys-checkbox/src/nys-checkbox.figma.ts
+++ packages/nys-checkbox/src/nys-checkbox.figma.ts
@@ -58,126 +58,144 @@ figma.connect("<FIGMA_CHECKBOXGROUP>", {
         false: undefined,
       }),
     }),
-    // error: figma.nestedProps("Error", {
-    //   message: figma.string("Error Message"),
-    // }),
-    error: figma.nestedProps("_Error", {
-      message: figma.string("Error Message"),
-    }),
     tile: figma.boolean("Tile"),
     input: figma.nestedProps("Input", {
+      /**
+       * Due to code connect limitation with nested instance within a nested instance,
+       * We can work around this with targeting the first level within "input" prop as usual, but putting the inner nested "error" prop outside of this key/value pair
+       */
       showError: figma.boolean("Error"),
-      // error: figma.nestedProps("_Error", {
-      //   message: figma.string("Error Message"),
-      // }),
+    }),
+    error: figma.nestedProps("Error", {
+      message: figma.string("Error Message"),
     }),
     checkbox_1: figma.nestedProps("Option 1", {
-      text: figma.string("Label"),
+      // text: figma.string("Label"),
+      // checked: figma.boolean("Checked"),
+      // disabled: figma.enum("State", {
+      //   Disabled: true,
+      // }),
+      // description: figma.boolean("Description", {
+      //   true: figma.string("↳ Description"),
+      // }),
+            text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_2: figma.nestedProps("Option 2", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_3: figma.nestedProps("Option 3", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_4: figma.nestedProps("Option 4", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_5: figma.nestedProps("Option 5", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_6: figma.nestedProps("Option 6", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_7: figma.nestedProps("Option 7", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_8: figma.nestedProps("Option 8", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_9: figma.nestedProps("Option 9", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
     checkbox_10: figma.nestedProps("Option 10", {
-      text: figma.string("Label"),
+      text: figma.string("↳ Label"),
+      description: figma.boolean("Multiline | Description", {
+        true: figma.boolean("Description", {
+          true: figma.string("↳ Description"),
+          false: undefined,
+        }),
+      }),
       checked: figma.boolean("Checked"),
-      disabled: figma.enum("State", {
-        Disabled: true,
-      }),
-      description: figma.boolean("Description", {
-        true: figma.string("↳ Description"),
-        false: undefined,
-      }),
+      disabled: figma.boolean("Disabled"),
+      required: figma.boolean("Required"),
     }),
   },
   example: (props) =>
@@ -190,90 +208,90 @@ figma.connect("<FIGMA_CHECKBOXGROUP>", {
         label="${props.label.text}"
         description="${props.label.description}"
         size="${props.size}"
-        ?required="${props.label.required}"
-        ?optional="${props.label.optional}"
-        ?showError="${props.input.showError}"
+        required="${props.label.required}"
+        optional="${props.label.optional}"
+        showError="${props.input.showError}"
         errorMessage="${props.error.message}"
         name="---REPLACE_THIS---"
-        ?tile="${props.tile}"
+        tile="${props.tile}"
       >
         <nys-checkbox
           label="${props.checkbox_1.text}"
           description="${props.checkbox_1.description}"
-          ?checked="${props.checkbox_1.checked}"
-          ?disabled="${props.checkbox_1.disabled}"
+          checked="${props.checkbox_1.checked}"
+          disabled="${props.checkbox_1.disabled}"
           value="${props.checkbox_1.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_2.text}"
           description="${props.checkbox_2.description}"
-          ?checked="${props.checkbox_2.checked}"
-          ?disabled="${props.checkbox_2.disabled}"
+          checked="${props.checkbox_2.checked}"
+          disabled="${props.checkbox_2.disabled}"
           value="${props.checkbox_2.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_3.text}"
           description="${props.checkbox_3.description}"
-          ?checked="${props.checkbox_3.checked}"
-          ?disabled="${props.checkbox_3.disabled}"
+          checked="${props.checkbox_3.checked}"
+          disabled="${props.checkbox_3.disabled}"
           value="${props.checkbox_3.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_4.text}"
           description="${props.checkbox_4.description}"
-          ?checked="${props.checkbox_4.checked}"
-          ?disabled="${props.checkbox_4.disabled}"
+          checked="${props.checkbox_4.checked}"
+          disabled="${props.checkbox_4.disabled}"
           value="${props.checkbox_4.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_5.text}"
           description="${props.checkbox_5.description}"
-          ?checked="${props.checkbox_5.checked}"
-          ?disabled="${props.checkbox_5.disabled}"
+          checked="${props.checkbox_5.checked}"
+          disabled="${props.checkbox_5.disabled}"
           value="${props.checkbox_5.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_6.text}"
           description="${props.checkbox_6.description}"
-          ?checked="${props.checkbox_6.checked}"
-          ?disabled="${props.checkbox_6.disabled}"
+          checked="${props.checkbox_6.checked}"
+          disabled="${props.checkbox_6.disabled}"
           value="${props.checkbox_6.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_7.text}"
           description="${props.checkbox_7.description}"
-          ?checked="${props.checkbox_7.checked}"
-          ?disabled="${props.checkbox_7.disabled}"
+          checked="${props.checkbox_7.checked}"
+          disabled="${props.checkbox_7.disabled}"
           value="${props.checkbox_7.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_8.text}"
           description="${props.checkbox_8.description}"
-          ?checked="${props.checkbox_8.checked}"
-          ?disabled="${props.checkbox_8.disabled}"
+          checked="${props.checkbox_8.checked}"
+          disabled="${props.checkbox_8.disabled}"
           value="${props.checkbox_8.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_9.text}"
           description="${props.checkbox_9.description}"
-          ?checked="${props.checkbox_9.checked}"
-          ?disabled="${props.checkbox_9.disabled}"
+          checked="${props.checkbox_9.checked}"
+          disabled="${props.checkbox_9.disabled}"
           value="${props.checkbox_9.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
         <nys-checkbox
           label="${props.checkbox_10.text}"
           description="${props.checkbox_10.description}"
-          ?checked="${props.checkbox_10.checked}"
-          ?disabled="${props.checkbox_10.disabled}"
+          checked="${props.checkbox_10.checked}"
+          disabled="${props.checkbox_10.disabled}"
           value="${props.checkbox_10.text}"
           name="---REPLACE_THIS---"
         ></nys-checkbox>
