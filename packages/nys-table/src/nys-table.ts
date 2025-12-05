e031eb674539e26424dfca3e41c38535447b4900
Emily Gorelik <emilygorelik2000@gmail.com>
2025-12-05 13:02:46 -0500

    download button works

--- packages/nys-table/src/nys-table.ts
+++ packages/nys-table/src/nys-table.ts
@@ -175,9 +175,47 @@ export class NysTable extends LitElement {
     btn.setAttribute("size", "sm");
     btn.setAttribute("prefixIcon", "download");
     btn.setAttribute("label", "Download");
-    //btn.onclick = () => this.tableToCSV();
+    btn.onclick = () => this.tableToCSV();
 
     container.appendChild(btn);
+  }
+
+  tableToCSV() {
+    const container = document.getElementById(this.id);
+    console.log("container:", container);
+    if (!container) return;
+
+    const table = container.querySelector("table");
+    if (!table) return;
+
+    let csv_data = [];
+
+    const rows = table.querySelectorAll("tr");
+    rows.forEach((row) => {
+      const cols = row.querySelectorAll("td,th");
+      const csvrow = Array.from(cols, (col) => col.innerHTML);
+      csv_data.push(csvrow.join(","));
+    });
+
+    const csvString = csv_data.join("\n");
+
+    this.downloadCSVFile(csvString);
+  }
+
+  downloadCSVFile(csv_data) {
+    let CSVFile = new Blob([csv_data], { type: "text/csv" });
+
+    let temp_link = document.createElement("a");
+
+    temp_link.download = `${this.download}.csv`;
+    let url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+
+    temp_link.click();
+    document.body.removeChild(temp_link);
   }
 
   /****************** Event Handlers ******************/
