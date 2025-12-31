48ec39061ae1c82cc9a6a441ef840685fb27c817
NovaCat35 <bergintonatcapital@gmail.com>
2025-12-30 22:43:51 -0500

    Code connect additions

--- packages/nys-datepicker/src/nys-datepicker.figma.ts
+++ packages/nys-datepicker/src/nys-datepicker.figma.ts
@@ -7,8 +7,23 @@ figma.connect("<FIGMA_DATEPICKER>", {
       lg: "lg",
       full: "full",
     }),
+    label: figma.nestedProps("Label", {
+      text: figma.string("Label"),
+      description: figma.boolean("Description", {
+        true: figma.string("↳ Description"),
+        false: undefined,
+      }),
+      required: figma.boolean("Required"),
+      optional: figma.boolean("Optional"),
+    }),
   },
   example: (props) => html`
-    <nys-datepicker width=${props.width}></nys-datepicker>
+    <nys-datepicker
+      label="${props.label.text}"
+      description="${props.label.description}"
+      width=${props.width}
+      required=${props.label.required}
+      optional=${props.label.optional}
+    ></nys-datepicker>
   `,
 });
