cef701b83581c11a0f7b426bea242d56615f678a
NovaCat35 <bergintonatcapital@gmail.com>
2026-01-22 09:28:32 -0500

    Fix checkbox description focus bug

--- packages/nys-checkbox/src/nys-checkbox.ts
+++ packages/nys-checkbox/src/nys-checkbox.ts
@@ -8,9 +8,8 @@ import styles from "./nys-checkbox.scss?inline";
 let checkboxIdCounter = 0; // Counter for generating unique IDs
 
 /**
- * `<nys-checkbox>` is a form-associated, accessible checkbox component.
- * Supports validation, labels, error messages, and keyboard interaction.
- * Can exist independently or inside a `<nys-checkboxgroup>`.
+ * `<nys-checkbox>` is a form-associated, accessible checkbox co88133
+ * .
  */
 export class NysCheckbox extends LitElement {
   static styles = unsafeCSS(styles);
@@ -182,12 +181,19 @@ export class NysCheckbox extends LitElement {
   }
 
   private _manageLabelClick = () => {
-    const labelEl = this.shadowRoot?.querySelector("nys-label");
+    const wrapper = this.shadowRoot?.querySelector(".nys-checkbox");
     const inputEl = this.shadowRoot?.querySelector("input");
 
-    if (labelEl && inputEl) {
-      labelEl.addEventListener("click", () => inputEl.click());
-    }
+    if (!wrapper || !inputEl) return;
+
+    wrapper.addEventListener("click", (e) => {
+      // Avoid double toggling the checkbox. Already handled by input
+      if ((e.target as HTMLElement).tagName.toLowerCase() === "input") return;
+
+      if (!this.disabled) {
+        inputEl.click();
+      }
+    });
   };
 
   /**
@@ -247,7 +253,7 @@ export class NysCheckbox extends LitElement {
 
   render() {
     return html`
-      <label class="nys-checkbox">
+      <div class="nys-checkbox">
         <div class="nys-checkbox__checkboxwrapper">
           <input
             id=${this.id + "--native"}
@@ -296,7 +302,7 @@ export class NysCheckbox extends LitElement {
             >
           </nys-label>
         `}
-      </label>
+      </div>
       ${this.parentElement?.tagName.toLowerCase() !== "nys-checkboxgroup"
         ? html`<nys-errormessage
             id="single-error-message"
