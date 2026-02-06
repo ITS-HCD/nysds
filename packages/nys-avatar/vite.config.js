bf82bf5d902a0774738c0169c1a0594c0d23db55
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2026-02-06 13:07:54 -0500

    DO NOT clean the output folder on build, we do this with clean:dist

--- packages/nys-avatar/vite.config.js
+++ packages/nys-avatar/vite.config.js
@@ -20,7 +20,7 @@ const overrideConfig = {
     lib: {
       fileName: () => "nys-avatar.js",
     },
-    emptyOutDir: true, // Since we're building both ES and UMD formats
+    
     rollupOptions: {
       output: [{ banner }],
     },
