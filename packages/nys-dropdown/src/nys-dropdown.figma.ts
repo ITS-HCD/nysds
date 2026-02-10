3a48881e1660eb2bf2850fb1f10a096c4b863f62
NovaCat35 <bergintonatcapital@gmail.com>
2026-02-10 14:13:13 -0500

    Remove dropdown

--- packages/nys-dropdown/src/nys-dropdown.figma.ts
+++
@@ -1,8 +0,0 @@
-import figma, { html } from "@figma/code-connect/html";
-
-figma.connect("<FIGMA_DROPDOWN>", {
-  props: {
-    // TODO: Add props here
-  },
-  example: () => html` <nys-dropdown></nys-dropdown> `,
-});
