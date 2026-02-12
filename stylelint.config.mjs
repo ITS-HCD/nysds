e49496740b10d36bd820406709231ae9e79b7b0d
Emily Gorelik <emily.gorelik@its.ny.gov>
2026-02-12 09:09:37 -0500

    lint fixes

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,6 +4,13 @@ export default {
     "custom-property-pattern": null,
     "selector-class-pattern": null,
     "selector-id-pattern": null,
+    "color-function-alias-notation": null,
+    "color-function-notation": null,
+    "alpha-value-notation": null,
+    "selector-not-notation": null,
+    "property-no-vendor-prefix": null,
+    "declaration-empty-line-before": null,
+    " declaration-block-no-redundant-longhand-properties": null,
     "color-hex-length": "long",
   },
   "extends": ["stylelint-config-standard-scss"]
