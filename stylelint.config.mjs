2fc2277b5962251c7be40301401dd4bd15167aae
Emily Gorelik <emily.gorelik@its.ny.gov>
2026-02-18 09:54:49 -0500

    fix linting, remove gen of mdx file

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,7 +3,15 @@ export default {
   "rules": {
     "custom-property-pattern": null,
     "selector-class-pattern": null,
-    "selector-id-pattern": null
+    "selector-id-pattern": null,
+    "color-function-alias-notation": null,
+    "color-function-notation": null,
+    "alpha-value-notation": null,
+    "selector-not-notation": null,
+    "property-no-vendor-prefix": null,
+    "declaration-empty-line-before": null,
+    "declaration-block-no-redundant-longhand-properties": null,
+    "color-hex-length": "long",
   },
   "extends": ["stylelint-config-standard-scss"]
 };
