2d5bfd89ed8b2cc3ba02dba4ea7a00f4b7973ff1
Emily Gorelik <emilygorelik2000@gmail.com>
2026-03-10 12:50:48 -0400

    wip

--- packages/nys-tab/src/nys-tab.test.ts
+++ packages/nys-tab/src/nys-tab.test.ts
@@ -1,12 +1,8 @@
 import { expect, html, fixture } from "@open-wc/testing";
 import "../dist/nys-tab.js";
 import { NysTab } from "./nys-tab.js";
+import { NysTabgroup } from "./nys-tabgroup.js";
 
-// You may need to import other dependencies such as the component's tag name
-// For example:
-// import { NysTextinput } from "./nys-textinput";
-
-// Below are placeholder examples of test cases for a web component. Add your own tests as needed.
 describe("nys-tab", () => {
   it("renders the component", async () => {
     const el = await fixture(html`<nys-tab></nys-tab>`);
@@ -21,24 +17,46 @@ describe("nys-tab", () => {
     expect(el.id).to.match(/^nys-tab-\d+-\d+$/);
   });
 
-  it("reflects attributes to properties", async () => {
-    const el = await fixture<NysTab>(html`
-      <nys-tab label="My Label" required optional></nys-tab>
-    `);
-    expect(el.label).to.equal("My Label");
-  });
-
   it("passes the a11y audit", async () => {
     const el = await fixture(html`<nys-tab label="My Label"></nys-tab>`);
     await expect(el).shadowDom.to.be.accessible();
   });
 
-  // Other test to consider:
-  // - Test for default values
-  // - Test for different attributes
-  // - Test for events
-  // - Test for methods
-  // - Test for accessibility
-  // - Test for slot content
-  // - Test for lifecycle methods
+  it("sets selected on the first tab if none have selected assigned", async () => {
+    const el = await fixture<NysTabgroup>(html`
+      <nys-tabgroup>
+        <nys-tab label="Tab One"></nys-tab>
+        <nys-tab label="Tab Two"></nys-tab>
+        <nys-tab label="Tab Three"></nys-tab>
+        <nys-tabpanel>Content for Tab One.</nys-tabpanel>
+        <nys-tabpanel>Content for Tab Two.</nys-tabpanel>
+        <nys-tabpanel>Content for Tab Three.</nys-tabpanel>
+      </nys-tabgroup>
+    `);
+    await el.updateComplete;
+
+    const tabs = el.querySelectorAll("nys-tab");
+    expect(tabs[0].hasAttribute("selected")).to.be.true;
+    expect(tabs[1].hasAttribute("selected")).to.be.false;
+    expect(tabs[2].hasAttribute("selected")).to.be.false;
+  });
+
+  it("removes selected from extra tabs if more than one has it assigned", async () => {
+    const el = await fixture<NysTabgroup>(html`
+      <nys-tabgroup>
+        <nys-tab label="Tab One" selected></nys-tab>
+        <nys-tab label="Tab Two" selected></nys-tab>
+        <nys-tab label="Tab Three" selected></nys-tab>
+        <nys-tabpanel>Content for Tab One.</nys-tabpanel>
+        <nys-tabpanel>Content for Tab Two.</nys-tabpanel>
+        <nys-tabpanel>Content for Tab Three.</nys-tabpanel>
+      </nys-tabgroup>
+    `);
+    await el.updateComplete;
+
+    const tabs = el.querySelectorAll("nys-tab");
+    expect(tabs[0].hasAttribute("selected")).to.be.true;
+    expect(tabs[1].hasAttribute("selected")).to.be.false;
+    expect(tabs[2].hasAttribute("selected")).to.be.false;
+  });
 });
