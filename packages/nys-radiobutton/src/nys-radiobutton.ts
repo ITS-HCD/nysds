d6ae4a68a98497e3c3fb6027b760750f610e8729
NovaCat35 <bergintonatcapital@gmail.com>
2026-03-10 14:44:16 -0400

    lint fix

--- packages/nys-radiobutton/src/nys-radiobutton.ts
+++ packages/nys-radiobutton/src/nys-radiobutton.ts
@@ -242,12 +242,16 @@ export class NysRadiobutton extends LitElement {
 
   // Handle focus event
   private _handleFocus() {
-    this.dispatchEvent(new Event("nys-focus", { bubbles: true, composed: true }));
+    this.dispatchEvent(
+      new Event("nys-focus", { bubbles: true, composed: true }),
+    );
   }
 
   // Handle blur event
   private _handleBlur() {
-    this.dispatchEvent(new Event("nys-blur", { bubbles: true, composed: true }));
+    this.dispatchEvent(
+      new Event("nys-blur", { bubbles: true, composed: true }),
+    );
 
     setTimeout(() => {
       // Only validate if we're blurring away from the component entirely
