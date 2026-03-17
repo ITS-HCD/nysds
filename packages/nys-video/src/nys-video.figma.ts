55d0f84c35998b33fa45a6aae8a9dd2c6f1ac264
NovaCat35 <bergintonatcapital@gmail.com>
2026-03-17 12:23:56 -0400

    Figma code connect fixes

--- packages/nys-video/src/nys-video.figma.ts
+++ packages/nys-video/src/nys-video.figma.ts
@@ -2,34 +2,23 @@ import figma, { html } from "@figma/code-connect/html";
 
 figma.connect("<FIGMA_VIDEO>", {
   props: {
-    videourl: figma.string("videourl"),
-    titleText: figma.string("titleText"),
-    size: figma.enum("size", {
-      full: "full",
-      contained: "contained",
-      compacted: "compacted",
+    titleText: figma.nestedProps("Title", {
+      text: figma.string("↳Text"),
     }),
-    arialabel: figma.string("arialabel"),
-    starttime: figma.string("starttime"),
-    thumbnail: figma.string("thumbnail"),
-    autoplay: figma.boolean("autoplay"),
-    disabled: figma.boolean("disabled"),
-    loading: figma.enum("loading", {
-      lazy: "lazy",
-      eager: "eager",
+    playButton: figma.nestedProps("Play Button", {
+      Disabled: figma.boolean("Disabled"),
+    }),
+    size: figma.enum("Size", {
+      "Full": "full",
+      "Contained": "contained",
+      "Compacted": "compacted",
     }),
   },
   example: (props) => html`
     <nys-video
-      videourl="${props.videourl}"
       size="${props.size}"
-      arialabel="${props.arialabel}"
-      titleText="${props.titleText}"
-      thumbnail="${props.thumbnail}"
-      starttime="${props.starttime}"
-      loading="${props.loading}"
-      ?autoplay="${props.autoplay}"
-      ?disabled="${props.disabled}"
+      titleText="${props.titleText.text}"
+      disabled="${props.playButton.Disabled}"
     ></nys-video>
   `,
 });
