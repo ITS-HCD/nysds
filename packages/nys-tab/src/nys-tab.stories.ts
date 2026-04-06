9fb413327411d97c9a8cd05c5ff715e8adbadeb7
Emily Gorelik <emilygorelik2000@gmail.com>
2026-04-06 12:51:25 -0400

    disabled in example

--- packages/nys-tab/src/nys-tab.stories.ts
+++ packages/nys-tab/src/nys-tab.stories.ts
@@ -33,48 +33,47 @@ type Story = StoryObj<NysTabArgs>;
 export const Basic: Story = {
   args: {},
   render: () => html`
-    <nys-tabgroup id="test">
-      <nys-tab label="Button"></nys-tab>
-      <nys-tab selected label="Some Accordions"></nys-tab>
-      <nys-tab label="Just text content in here"></nys-tab>
-      <nys-tab disabled label="I am disabled and you can't get to me"></nys-tab>
-      <nys-tabpanel>
-        Content for Tab One.
-        <nys-button label="test"></nys-button
-      ></nys-tabpanel>
-      <nys-tabpanel>
-        <nys-accordion>
-          <nys-accordionitem
-            id="accordionId1"
-            heading="How do I renew my passport or apply for a new one?"
-          >
-            <p>
-              You can apply for or renew a U.S. passport through the U.S.
-              Department of State. Some renewals can be done by mail.
-            </p>
-            <div style="display: flex; gap: 0.5rem; font-size: 1rem;">
-              <a href="https://www.ny.gov" target="_blank"
-                >Check your registration</a
-              >
-              <a href="https://www.ny.gov" target="_blank"
-                >Fill out application</a
-              >
-            </div>
-          </nys-accordionitem>
-          <nys-accordionitem
-            id="accordionId2"
-            heading="How can I find out if I'm registered to vote?"
-          >
-            <p>
-              You can check your registration status, update your information,
-              or find out how to register through the National Association of
-              Secretaries of State.
-            </p>
-          </nys-accordionitem>
-        </nys-accordion>
+    <nys-tabgroup>
+      <nys-tab label="Marcy"></nys-tab>
+      <nys-tab label="Algonquin"></nys-tab>
+      <nys-tab label="Haystack"></nys-tab>
+      <nys-tab label="Skylight" id="tab4"></nys-tab>
+      <nys-tab label="Whiteface" id="tab5"></nys-tab>
+      <nys-tab label="Gore (not a high peak)" disabled id="tab6"></nys-tab>
+      <nys-tabpanel
+        >Mount Marcy is the tallest peak in the Adirondacks. It has an elevation
+        of 5344 feet with 3166 feet of elevation gain. The roundtrip hike to the
+        top is 14.8 miles and is an out-and-back route.
       </nys-tabpanel>
-      <nys-tabpanel>Content for Tab Three.</nys-tabpanel>
-      <nys-tabpanel>Content for Tab Four.</nys-tabpanel>
+      <nys-tabpanel
+        >Algonquin Peak is the 2nd tallest peak in the Adirondacks. It has an
+        elevation of 5114 feet with 3050 feet of elevation gain. The roundtrip
+        hike to the top is 7.8 miles and is an out-and-back route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Mount Haystack is the 3rd tallest peak in the Adirondacks. It has an
+        elevation of 4960 feet with 4000 feet of elevation gain. The roundtrip
+        hike to the top is 16.4 miles and is an out-and-back
+        route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Mount Skylight is the 4th tallest peak in the Adirondacks. It has an
+        elevation of 4926 feet with 5100 feet of elevation gain. The roundtrip
+        hike to the top is 16 miles and is an out-and-back route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Whiteface is the 5th tallest peak in the Adirondacks. It has an
+        elevation of 4867 feet with 3050 feet of elevation gain. The roundtrip
+        hike to the top is 7.2 miles and is an out-and-back route.</nys-tabpanel
+      >
+      <p style="padding: 1rem;">
+        <strong>Note:</strong> The content in the tab panels was gathered from
+        <a href="https://www.lakeplacid.com/do/outdoors/summerfall/hiking"
+          >lakeplacid.com</a
+        >
+        and may not be completely accurate. This is meant to be a demo of how
+        the component works, not a hiking guide.
+      </p>
     </nys-tabgroup>
   `,
   parameters: {
@@ -82,13 +81,39 @@ export const Basic: Story = {
       source: {
         code: `
 <nys-tabgroup>
-  <nys-tab label="Tab One"></nys-tab>
-  <nys-tabpanel>Content for Tab One.</nys-tabpanel>
-  <nys-tab label="Tab Two"></nys-tab>
-  <nys-tabpanel>Content for Tab Two.</nys-tabpanel>
-  <nys-tab label="Tab Three"></nys-tab>
-  <nys-tabpanel>Content for Tab Three.</nys-tabpanel>
-</nys-tabgroup>`,
+      <nys-tab label="Marcy"></nys-tab>
+      <nys-tab label="Algonquin"></nys-tab>
+      <nys-tab label="Haystack"></nys-tab>
+      <nys-tab label="Skylight" id="tab4"></nys-tab>
+      <nys-tab label="Whiteface" id="tab5"></nys-tab>
+      <nys-tab label="Gore (not a high peak)" disabled id="tab6"></nys-tab>
+      <nys-tabpanel
+        >Mount Marcy is the tallest peak in the Adirondacks. It has an elevation
+        of 5344 feet with 3166 feet of elevation gain. The roundtrip hike to the
+        top is 14.8 miles and is an out-and-back route.
+      </nys-tabpanel>
+      <nys-tabpanel
+        >Algonquin Peak is the 2nd tallest peak in the Adirondacks. It has an
+        elevation of 5114 feet with 3050 feet of elevation gain. The roundtrip
+        hike to the top is 7.8 miles and is an out-and-back route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Mount Haystack is the 3rd tallest peak in the Adirondacks. It has an
+        elevation of 4960 feet with 4000 feet of elevation gain. The roundtrip
+        hike to the top is 16.4 miles and is an out-and-back
+        route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Mount Skylight is the 4th tallest peak in the Adirondacks. It has an
+        elevation of 4926 feet with 5100 feet of elevation gain. The roundtrip
+        hike to the top is 16 miles and is an out-and-back route.</nys-tabpanel
+      >
+      <nys-tabpanel
+        >Whiteface is the 5th tallest peak in the Adirondacks. It has an
+        elevation of 4867 feet with 3050 feet of elevation gain. The roundtrip
+        hike to the top is 7.2 miles and is an out-and-back route.</nys-tabpanel
+      >
+    </nys-tabgroup>`,
         type: "auto",
       },
     },
