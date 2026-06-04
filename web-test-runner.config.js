7deb477fc934dd80b472fec9d6bdaa19b13fedc2
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2026-06-04 15:52:27 -0400

    reduce to 75

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -38,21 +38,18 @@ export default {
       product: "chromium",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     }),
     playwrightLauncher({
       product: "webkit",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     }),
     playwrightLauncher({
       product: "webkit",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices["iPhone 14"], hasTouch: true });
@@ -62,7 +59,6 @@ export default {
       product: "webkit",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices["Pixel 5"], hasTouch: true });
@@ -72,7 +68,6 @@ export default {
       product: "webkit",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
       createBrowserContext({ browser }) {
         return browser.newContext({
@@ -85,7 +80,6 @@ export default {
       product: "firefox",
       launchOptions: {
         headless: true,
-        args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     }),
   ],
