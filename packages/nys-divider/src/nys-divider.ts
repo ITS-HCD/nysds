99f070b267a94750250dcc6ba8e53915c1a6339b
Emily Gorelik <emily.gorelik@its.ny.gov>
2026-06-09 09:35:13 -0400

    reset

--- packages/nys-divider/src/nys-divider.ts
+++ packages/nys-divider/src/nys-divider.ts
@@ -8,31 +8,6 @@ let dividerIdCounter = 0;
 /**
  * A horizontal rule for visual separation between content sections. Renders a semantic `<hr>` element.
  *
- * ## When to use
- * - To separate content displayed vertically
- * - Use `inverted` property when placing the component on a dark background
- *
- * Avoid for:
- * - When content is horizontally formatted
- *
- * @accessibility
- * - Uses native `<hr>` element to ensure ARIA compliance
- * - Recognized as a separator by assistive technologies
- *
- * ## Do's and Don'ts
- * **Do:**
- * - Ensure consistent padding/margin above and below dividers to avoid crowding.
- * - Use the `inverted` option on dark backgrounds.
- *
- * **Don't:**
- * - Overuse dividers and clutter the interface, reducing scannability.
- * - Use the divider to separate content appearing side by side.
- * - Use the `inverted` option on light backgrounds.
- *
- * ## Dependencies
- *
- * This component has no dependencies on other NYS Design System components.
- *
  * @summary Horizontal divider for visual separation of content sections.
  * @element nys-divider
  *
