b5ad2ec5ce81142ad3730bc22cb18e23f1064dfc
NovaCat35 <bergintonatcapital@gmail.com>
2026-06-19 11:28:07 -0400

    Add code connect generics

--- packages/nys-tab/src/nys-tab.figma.ts
+++ packages/nys-tab/src/nys-tab.figma.ts
@@ -1,15 +1,19 @@
 import figma, { html } from "@figma/code-connect/html";
 
+// figma.connect("<FIGMA_TAB>", {
+//   props: {
+//     label: figma.string("Label"),
+//     active: figma.boolean("Active"),
+//     disabled: figma.boolean("Disabled"),
+//   },
+//   example: (props) =>
+//     html`<nys-tab
+//       label="${props.label}"
+//       ?selected="${props.active}"
+//       ?disabled="${props.disabled}"
+//     ></nys-tab>`,
+// });
+
 figma.connect("<FIGMA_TAB>", {
-  props: {
-    label: figma.string("Label"),
-    active: figma.boolean("Active"),
-    disabled: figma.boolean("Disabled"),
-  },
-  example: ({ label, active, disabled }) =>
-    html`<nys-tab
-      label="${label}"
-      ?selected="${active}"
-      ?disabled="${disabled}"
-    ></nys-tab>`,
+  example: () => html` <nys-tab label="Tab label" selected></nys-tab> `,
 });
