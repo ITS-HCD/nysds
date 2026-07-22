0ade87993c4f5145b4a3c092841aa44c28fba510
Emily Gorelik <emily.gorelik@its.ny.gov>
2026-07-22 13:53:40 -0400

    address PR comments

--- packages/nys-iconlist/src/nys-iconlist.figma.ts
+++ packages/nys-iconlist/src/nys-iconlist.figma.ts
@@ -4,5 +4,10 @@ figma.connect("<FIGMA_ICONLIST>", {
   props: {
     // TODO: Add props here
   },
-  example: () => html` <nys-iconlist></nys-iconlist> `,
+  example: () =>
+    html` <nys-iconlist id="event-details">
+      <nys-iconlistitem icon="calendar_month">July 4, 2026</nys-iconlistitem>
+      <nys-iconlistitem icon="schedule">5:00</nys-iconlistitem>
+      <nys-iconlistitem icon="location_on">Central Park West</nys-iconlistitem>
+    </nys-iconlist>`,
 });
