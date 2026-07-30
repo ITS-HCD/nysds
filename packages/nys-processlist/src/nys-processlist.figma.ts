01d974a72960999688ba528832ca12155d467cf0
Emily Gorelik <emily.gorelik@its.ny.gov>
2026-07-30 11:20:45 -0400

    basic figma

--- packages/nys-processlist/src/nys-processlist.figma.ts
+++ packages/nys-processlist/src/nys-processlist.figma.ts
@@ -4,5 +4,14 @@ figma.connect("<FIGMA_PROCESSLIST>", {
   props: {
     // TODO: Add props here
   },
-  example: () => html` <nys-processlist></nys-processlist> `,
+  example: () =>
+    html` <nys-processlist id="application-steps">
+      <nys-processlistitem label="Gather your documents"></nys-processlistitem>
+      <nys-processlistitem
+        label="Complete the application"
+      ></nys-processlistitem>
+      <nys-processlistitem
+        label="Submit and await review"
+      ></nys-processlistitem>
+    </nys-processlist>`,
 });
