6bc2b9be03efd9c255ffd417b28d6bf262a2d799
NovaCat35 <bergintonatcapital@gmail.com>
2026-09-14 17:39:22 -0400

    Fix issue with centered icon for action links only

--- packages/nys-alert/src/nys-alert.ts
+++ packages/nys-alert/src/nys-alert.ts
@@ -288,7 +288,9 @@ export class NysAlert extends NysElement {
       ${!this._alertClosed
         ? html` <div
             class="nys-alert__container ${this._slotHasContent ||
-            this.text?.trim().length > 0
+            this.text?.trim().length > 0 ||
+            this.primaryAction ||
+            this.secondaryAction
               ? ""
               : "nys-alert--centered"}"
           >
